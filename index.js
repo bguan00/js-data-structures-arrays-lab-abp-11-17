7125b876cdfd6fb30d2e0d97a098b49272e5f6eb
Bo Guan <bguan00@gmail.com>
2017-11-17 18:47:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function appendDriver (name) {
   newArray.push(name)
   return newArray
 }
-function prependDriver (name) {drivers.splice(0,0,name)}
+function prependDriver (name) {const driverscopy=drivers.slice();
+driverscopy.slice(name); return driverscopy}
 function removeLastDriver (name) {drivers.slice(0, drivers.length-1)}
 function removeFirstDriver (name) {drivers.slice(1)}
