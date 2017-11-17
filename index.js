2d16877ccfa1b8d0481b4dadf7c00f8dabc4b3d8
Bo Guan <bguan00@gmail.com>
2017-11-17 18:02:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your solution here!
 const drivers=["Milo", "Otis","Garfield"]
-function destructivelyAppendDriver () {drivers.push()}
+function destructivelyAppendDriver (name) {drivers.push(name)}
