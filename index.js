eb24657fd75d0a48ac1ae0097d50089057630fa4
Bo Guan <bguan00@gmail.com>
2017-11-17 18:07:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 const drivers=["Milo", "Otis","Garfield"]
 function destructivelyAppendDriver (name) {drivers.push(name)}
 function destructivelyPrependDriver (name) {drivers.unshift(name)}
-function destructivelyRemoveLastDriver (name) {drivers.push(name)}
+function destructivelyRemoveLastDriver (name) {drivers.shift(name)}
 function destructivelyRemoveFirstDriver (name) {drivers.push(name)}
+function appendDriver (name) {drivers.push(name)}
+function prependDriver (name) {drivers.push(name)}
+function removeLastDriver (name) {drivers.push(name)}
+function removeFirstDriver (name) {drivers.push(name)}
